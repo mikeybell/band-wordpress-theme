094ae7b23230dcf8d2eb949c58de581981cae434
mikeybell <michaelrobertbell@gmail.com>
2015-10-27 18:07:06 -0400

    Worked on nav

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 	<?php // Load Meta ?>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   <title><?php  wp_title('|', true, 'right'); ?></title>
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
@@ -17,17 +18,22 @@
 <body <?php body_class(); ?>>
 
 <header>
-  <div class="container">
-    <h1>
-      <a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name', 'display' ); ?>" rel="home">
-        <?php bloginfo( 'name' ); ?>
-      </a>
-    </h1>
 
-    <?php wp_nav_menu( array(
-      'container' => false,
-      'theme_locations' => 'primary'
-    )); ?>
+  <div class="container">
+    <div class="nav">
+      
+        <div class="nav-right">
+        <?php wp_nav_menu( array(
+          'container' => false,
+          'theme_location' => 'primary'
+        )); ?>
+        </div> <!-- /.nav-right -->
+
+    </div> <!-- /.nav-->
+
+
+    
   </div> <!-- /.container -->
+
 </header><!--/.header-->
 
