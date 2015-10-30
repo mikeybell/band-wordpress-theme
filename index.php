f2d6675ffeb2c4ccb840b8b3276d1854999b94ee
mikeybell <michaelrobertbell@gmail.com>
2015-10-30 09:03:45 -0400

    work on index

--- index.php
+++ index.php
@@ -1,40 +1,16 @@
 <?php //index.php is the last resort template, if no other templates match ?>
 <?php get_header(); ?>
 
-<div class="main-index">
+<div class="main-index clearfix">
   <div class="container">
 
-  	<?php 
-  	// the query
-  	$the_query = new WP_Query( array(
-  		'posts_per_page' => 100
-  	) ); ?>
+    <div class="content">
+        <?php get_template_part( 'loop', 'index' ); ?>
+    </div> <!--/.content -->
 
-  	<?php if ( $the_query->have_posts() ) : ?>
-
-  		<!-- pagination here -->
-
-  		<!-- the loop -->
-  		<div class="home-posts">
-  			<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-  				<div class="post">
-  					<div class="line"></div>
-  					<?php the_post_thumbnail('large'); ?>
-  					<h4><?php the_title(); ?></h4>
-  					<div class="white"><?php the_excerpt(); ?></div> <!-- /.white -->
-  				</div> <!-- /.post -->
-  			<?php endwhile; ?>
-  		</div> <!-- /.home-posts -->
-  		<!-- end of the loop -->
-
-  		<?php wp_reset_postdata(); ?>
-
-  	<?php else : ?>
-  		<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-  	<?php endif; ?>
-
+    <?php get_sidebar(); ?>
 
   </div> <!-- /.container -->
-</div> <!-- /.main-index -->
+</div> <!-- /.main -->
 
 <?php get_footer(); ?>
